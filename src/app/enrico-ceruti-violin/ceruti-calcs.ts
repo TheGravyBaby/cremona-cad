6e42b4543b452683ddcbee3de803fc462cb8131a
Andrew <aargraves@berkeley.edu>
2026-05-18 00:58:59 -0400

    C0 bugs

--- src/app/enrico-ceruti-violin/ceruti-calcs.ts
+++ src/app/enrico-ceruti-violin/ceruti-calcs.ts
@@ -296,21 +296,24 @@ export function calculateCenterBout(p: EnricoCerutiParams, solveC0?: boolean): v
             // now we need to convert the angle to the standard xy plane
             let lineFromU2toL2 = lineFromTwoPoints(p.bouts.U2!, p.bouts.L2!);
             if (lineFromU2toL2.m !== Infinity && lineFromU2toL2.m !== -Infinity) {
-                let angleFromU2toL2 = Math.atan(lineFromU2toL2.m); // angle of the line from U2 to L2, with the standard xy plane as reference
-                let diffFromYAxis = Math.PI / 2 - angleFromU2toL2; // angle from the line U2 to L2, to the Y axis
-                theta = theta - diffFromYAxis; // angle from U2 to C0, with the standard xy plane as reference
+                let angleFromU2toL2 = Math.atan2(p.bouts.L2!.y - p.bouts.U2!.y, p.bouts.L2!.x - p.bouts.U2!.x);
+                let diffFromYAxis = Math.PI / 2 + angleFromU2toL2; // angle from the line U2 to L2, to the Y axis
+                theta = theta + diffFromYAxis; // angle from U2 to C0, with the standard xy plane as reference
             }
 
             // cos(finalAngle) = o / r = (C0.x - U2.x) / (C0.r + U2.r)
             // sin(finalAngle) = a / r = (U2.y - C0.y) / (C0.r + U2.r)
-            p.bouts.C0.x = p.bouts.U2.x + Math.cos(theta) * UtoC;
-            p.bouts.C0.y = p.bouts.U2.y + Math.sin(theta) * UtoC;
+            p.bouts.C0.x = Math.abs(p.bouts.U2.x + Math.cos(theta) * UtoC);
+            p.bouts.C0.y = Math.abs(p.bouts.U2.y + Math.sin(theta) * UtoC);
 
             // if the above calculations result in a C0 that doesn't intersect with L2, then we have an impossible C0 and should throw an error
             let C0toL2 = dist(p.bouts.C0, p.bouts.L2!);
             let C0toU2 = dist(p.bouts.C0, p.bouts.U2!);
-            if (C0toL2 > p.bouts.C0.r + p.bouts.L2.r + 1 || C0toU2 > p.bouts.C0.r + p.bouts.U2.r + 1)
-                throw new Error("Given the radii of C0, L2 and U2, there is no condition where C0 can be fit. Likely, you can increase the radius of C0 and try again.")
+            let C0rU2r = p.bouts.C0.r + p.bouts.L2.r
+            let C0rL2r = p.bouts.C0.r + p.bouts.U2.r
+            let tolerance = 2
+            if (C0toL2 > C0rU2r + tolerance || C0toU2 > C0rL2r + tolerance)
+                throw new Error("Given the radii of C0, L2 and U2, there is no condition where C0 can be fit. Likely, you can increase the radius of C0 and try again.");
 
             // makes sense to recalculate center bout width here
             p.bouts.CBW = (p.bouts.C0.x - p.bouts.C0.r + inset) * 2;
